275bcf141d1e117e86442516fc0a5ba265f8dfc0
Richard Haines <hello@richardhaines.dev>
2021-09-01 20:25:34 +0200

    added packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from '../utlis/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
