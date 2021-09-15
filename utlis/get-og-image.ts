518bac7a4ad274215952207596526ad8c9bee057
Rich Haines <hello@richardhaines.dev>
2021-09-15 15:23:49 +0200

    swapped packages

--- utlis/get-og-image.ts
+++ utlis/get-og-image.ts
@@ -1,26 +1,24 @@
-const playwright = require('playwright-aws-lambda');
-const { createHash } = require('crypto');
-const fs = require('fs');
+import chrome from 'chrome-aws-lambda';
+import { createHash } from 'crypto';
+import fs from 'fs';
 
 async function getOgImage(path, baseUrl = 'https://richardhaines-og-image.vercel.app/') {
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   return 'og image will be generated in production';
-  // }
+  if (process.env.NODE_ENV === 'development') {
+    return 'og image will be generated in production';
+  }
 
   const url = `${baseUrl}${path}`;
   const hash = createHash('md5').update(url).digest('hex');
-  const browser = await playwright.launchChromium({ headless: true });
   const ogImageDir = `./public/og-images`;
   const imagePath = `${ogImageDir}/${hash}.png`;
   const publicPath = `${process.env.BASE_URL}/og-images/${hash}.png`;
 
-  try {
-    fs.statSync(imagePath);
-    return publicPath;
-  } catch (error) {
-    // file does not exists, so we create it
-  }
+  const browser = await chrome.puppeteer.launch({ 
+    args: chrome.args,
+    headless: true,
+    executablePath: await chrome.executablePath,
+  });
 
   const page = await browser.newPage();
   await page.setViewportSize({ width: 1200, height: 630 });
