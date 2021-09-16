91b64d013b56192b842c6f52e42caca9335e8e6d
Richard Haines <hello@richardhaines.dev>
2021-09-16 20:28:09 +0200

    get build working

--- api/get-og-image.ts
+++ api/get-og-image.ts
@@ -1,4 +1,4 @@
-  
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 import chrome from 'chrome-aws-lambda';
 import playwright from 'playwright';
@@ -9,7 +9,7 @@ import fs from 'fs';
 const BASE_URL = 'https://richardhaines-og-image.vercel.app'
 
 export default async function getOgImage(res: NextApiResponse, req: NextApiRequest) {
-  const { body: { path }} = req
+  const { body: { path } } = req
   console.log({ path })
 
   // if (process.env.NODE_ENV === 'development') {
@@ -18,50 +18,50 @@ export default async function getOgImage(res: NextApiResponse, req: NextApiReque
 
   try {
 
-  const browser = await playwright.chromium.launch({ 
-    // args: chrome.args,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    // executablePath: await chrome.executablePath || "C:\\Users\\richa\\AppData\\Local\\ms-playwright\\chromium-907428\\chrome-win\\chrome.exe",
-    // executablePath: await chrome.executablePath || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    headless: chrome.headless,
-  });
+    // const browser = await playwright.chromium.launch({
+    //   // args: chrome.args,
+    //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    //   // executablePath: await chrome.executablePath || "C:\\Users\\richa\\AppData\\Local\\ms-playwright\\chromium-907428\\chrome-win\\chrome.exe",
+    //   // executablePath: await chrome.executablePath || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    //   headless: chrome.headless,
+    // });
 
-  const url = `${BASE_URL}${path}`;
-  const hash = createHash('md5').update(url).digest('hex');
-  const ogImageDir = `./public/og-images`;
-  const imagePath = `${ogImageDir}/${hash}.png`;
-  const publicPath = `${BASE_URL}/og-images/${hash}.png`;
+    const url = `${BASE_URL}${path}`;
+    const hash = createHash('md5').update(url).digest('hex');
+    const ogImageDir = `./public/og-images`;
+    const imagePath = `${ogImageDir}/${hash}.png`;
+    const publicPath = `${BASE_URL}/og-images/${hash}.png`;
 
-  console.log({publicPath})
+    console.log({ publicPath })
 
-  try {
-    if (fs.existsSync(imagePath)) {
-      res.send(publicPath);
+    try {
+      if (fs.existsSync(imagePath)) {
+        res.send(publicPath);
+      }
+    } catch (e) {
+      res.send(`There was an error: ${e.message}`)
     }
+
+    // const page = await browser.newPage({
+    //   viewport: {
+    //     width: 1200,
+    //     height: 630
+    //   }
+    // });
+    // await page.goto(url, {
+    //   timeout: 15 * 1000
+    // })
+    // const buffer = await page.screenshot({ type: 'png' }) as Buffer;
+    // await browser.close();
+
+    // fs.mkdirSync(ogImageDir, { recursive: true });
+    // fs.writeFileSync(imagePath, buffer);
+
+    // return publicPath;
+    res.send(publicPath);
   } catch (e) {
     res.send(`There was an error: ${e.message}`)
+    // return `There was an error: ${e.message}`;
   }
-  
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 630
-    }
-  });
-  await page.goto(url, {
-    timeout: 15 * 1000
-  })
-  const buffer = await page.screenshot({ type: 'png' }) as Buffer;
-  await browser.close();
-
-  fs.mkdirSync(ogImageDir, { recursive: true });
-  fs.writeFileSync(imagePath, buffer);
-
-  // return publicPath;
-  res.send(publicPath);
- } catch(e) {
-  res.send(`There was an error: ${e.message}`)
-  // return `There was an error: ${e.message}`;
- }
 }
 
