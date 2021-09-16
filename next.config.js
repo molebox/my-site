cb39714920918b7a92fcd634c643a444cbe82e36
Richard Haines <hello@richardhaines.dev>
2021-09-16 21:03:14 +0200

    redirects wrong

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: `/`,
-        destination: '/writing/',
-        permanent: true
-      }
-    ]
-  }
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: `/`,
+  //       destination: '/writing/',
+  //       permanent: true
+  //     }
+  //   ]
+  // }
 }
