024578fa75860d75391cbbf370782259373c5238
Rich Haines <hello@richardhaines.dev>
2021-09-16 12:24:15 +0200

    logging..6

--- utlis/get-og-image.ts
+++ utlis/get-og-image.ts
@@ -19,7 +19,8 @@ export default async function getOgImage(path: string) {
   try {
 
   const browser = await playwright.chromium.launch({ 
-    args: chrome.args,
+    // args: chrome.args,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
     // executablePath: await chrome.executablePath || "D:\\chrome-win\\chrome.exe",
     // executablePath: await chrome.executablePath || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
     headless: chrome.headless,
