a0d2c227f9f74a74f1726927ac5aef56b2eb24b2
Rich Haines <hello@richardhaines.dev>
2021-09-16 11:30:08 +0200

    changed function

--- src/pages/api/get-og-image.ts
+++ src/pages/api/get-og-image.ts
@@ -1,42 +1,50 @@
+  
+import type { NextApiRequest, NextApiResponse } from 'next'
 import chrome from 'chrome-aws-lambda';
-// import puppeteer from 'puppeteer';
+import playwright from 'playwright-core';
 import { createHash } from 'crypto';
 import fs from 'fs';
 // path, baseUrl = 'https://richardhaines-og-image.vercel.app/'
 
 const BASE_URL = 'https://richardhaines-og-image.vercel.app/'
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { body: { path } } = req;
 
-  if (process.env.NODE_ENV === 'development') {
-    return 'og image will be generated in production';
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   return 'og image will be generated in production';
+  // }
 
   try {
+
+  const browser = await playwright.chromium.launch({ 
+    args: chrome.args,
+    executablePath: await chrome.executablePath || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    headless: chrome.headless,
+  });
+
   const url = `${BASE_URL}${path}`;
   const hash = createHash('md5').update(url).digest('hex');
   const ogImageDir = `./public/og-images`;
   const imagePath = `${ogImageDir}/${hash}.png`;
   const publicPath = `${process.env.BASE_URL}/og-images/${hash}.png`;
 
-  if (fs.existsSync(imagePath)) {
-    res.send({
-      status: 200,
-      publicPath
-    });
-  }
+  // if (fs.existsSync(imagePath)) {
+  //   res.send({
+  //     status: 200,
+  //     publicPath
+  //   });
+  // }
 
-  const browser = await chrome.puppeteer.launch({ 
-    args: [...chrome.args, "--disable-web-security"],
-    executablePath: await chrome.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 630
+    }
   });
-
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 630 });
-  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.goto(url, {
+    timeout: 15 * 1000
+  })
   const buffer = await page.screenshot({ type: 'png' }) as Buffer;
   await browser.close();
 
